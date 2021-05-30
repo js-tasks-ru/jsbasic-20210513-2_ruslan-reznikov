6bffb07ab1a8e08c28e2d047b2576ede5332cbf0
Ruslan Reznikov <ruslanreznikov8gmail.com>
2021-05-30 16:03:48 +0300

    tasks

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
 function getMinMax(str) {
   // ваш код...
+
+  let arrs = str.split(/[ ,]/);
+  let result = arrs
+    .map(item => Number(item))
+    .filter(item => !isNaN(item));
+
+  return {
+    min: Math.min.apply(null, result),
+    max: Math.max.apply(null, result)
+  };
 }
